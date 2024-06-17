88ce2a2f2d3820f91d3aec3aff9e7a84b05d4163
Erudite098 <macalde.kerzania1029@gmail.com>
2024-06-17 16:15:59 +0800

    added connection to database

--- src/test.java
+++ src/test.java
@@ -2,6 +2,7 @@ import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -12,13 +13,18 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
+import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
+
+import net.proteanit.sql.DbUtils;
+
+
 
 public class test extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField txtFieldUserName;
+	private JTextField txtsurname;
 	private JPasswordField passFieldLogin;
 
 	/**
@@ -36,51 +42,120 @@ public class test extends JFrame {
 			}
 		});
 	}
+	
+	public test() {
+		testMe();
+		Connect();
+		table_load();
+	}
+	
+	Connection con;
+	PreparedStatement pst;
+	ResultSet rs;
+	private JTable table;
+	private JTextField textfullname;
+	
+	public void Connect() {
+		try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/infoman", "root","");
+        }
+        catch (ClassNotFoundException ex) 
+        {
+          ex.printStackTrace();
+        }
+        catch (SQLException ex) 
+        {
+           ex.printStackTrace();
+        }
+	}
+	
+	public void table_load(){
+	    try {
+	    	pst = con.prepareStatement("select * from employee");
+	    	rs = pst.executeQuery();
+	    	table.setModel(DbUtils.resultSetToTableModel(rs));
+	    } 
+	    catch (SQLException e) {
+	        e.printStackTrace();
+	    } 
+	}
+	
+	
 
 	/**
 	 * Create the frame.
 	 */
-	public test() {
+	private void testMe() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 601, 442);
+		setBounds(100, 100, 833, 638);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		JButton btnLogin = new JButton("Login");
-		btnLogin.addActionListener(new ActionListener() {
+		JButton btnAdd = new JButton("add");
+		btnAdd.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String username = txtFieldUserName.getText();
-				String password = new String(passFieldLogin.getPassword());
-				if (authenticate(username, password)) {
-					// Proceed to main application
-				} else {
-					JOptionPane.showMessageDialog(null, "Invalid credentials");
-				}
+				String surname, fullname;
+			    surname = txtsurname.getText();
+			    fullname = textfullname.getText();
+	
+			                
+			     try {
+			        pst = con.prepareStatement("insert into employee(surname, fullname)values(?,?)");
+			        pst.setString(1, surname);
+			        pst.setString(2, fullname);
+			
+			        pst.executeUpdate();
+			        JOptionPane.showMessageDialog(null, "Record Addedddd!!!!!");
+			        table_load();
+			                       
+			        txtsurname.setText("");
+			        textfullname.setText("");
+//			        txtPrice.setText("");
+			        txtsurname.requestFocus();
+			       }
+			    catch (SQLException e1) 
+			        {            
+			       e1.printStackTrace();
+			    }
 			}
 		});
 
-		btnLogin.setBounds(203, 166, 96, 21);
-		contentPane.add(btnLogin);
+		btnAdd.setBounds(203, 291, 96, 21);
+		contentPane.add(btnAdd);
 
-		txtFieldUserName = new JTextField();
-		txtFieldUserName.setColumns(10);
-		txtFieldUserName.setBounds(203, 82, 200, 30);
-		contentPane.add(txtFieldUserName);
+		txtsurname = new JTextField();
+		txtsurname.setColumns(10);
+		txtsurname.setBounds(203, 51, 200, 30);
+		contentPane.add(txtsurname);
 
 		JLabel lblUserName = new JLabel("UserName");
-		lblUserName.setBounds(203, 65, 125, 15);
+		lblUserName.setBounds(203, 34, 125, 15);
 		contentPane.add(lblUserName);
 
 		JLabel lblPassword = new JLabel("Password");
-		lblPassword.setBounds(203, 111, 70, 15);
+		lblPassword.setBounds(203, 199, 70, 15);
 		contentPane.add(lblPassword);
 
 		passFieldLogin = new JPasswordField();
-		passFieldLogin.setBounds(203, 124, 200, 30);
+		passFieldLogin.setBounds(203, 224, 200, 30);
 		contentPane.add(passFieldLogin);
+		
+		table = new JTable();
+		table.setBounds(160, 397, 381, 120);
+		contentPane.add(table);
+		
+		textfullname = new JTextField();
+		textfullname.setColumns(10);
+		textfullname.setBounds(203, 108, 200, 30);
+		contentPane.add(textfullname);
+		
+		JLabel lblUserName_1 = new JLabel("UserName");
+		lblUserName_1.setBounds(203, 91, 125, 15);
+		contentPane.add(lblUserName_1);
 	}// end frame
 
 	private boolean authenticate(String username, String password) {
